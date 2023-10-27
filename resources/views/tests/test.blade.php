4674c54bf54388e17f3bf91499e4b209905291d1
HTakuto <htakutolife@gmail.com>
2023-10-27 16:43:12 +0900

    javascriptの切り離し

--- resources/views/tests/test.blade.php
+++ resources/views/tests/test.blade.php
@@ -14,38 +14,6 @@
     @include('layouts.hamburger')
     @include('layouts.main')
 
-    <script>
-        // JavaScript for handling the hamburger menu
-        const hamburgerIcon = document.querySelector('.hamburger-icon');
-        const mobileMenu = document.querySelector('.mobile-menu');
-
-        hamburgerIcon.addEventListener('click', (event) => {
-            event.stopPropagation();
-            mobileMenu.classList.toggle('active');
-        });
-
-        mobileMenu.addEventListener('click', (event) => {
-            event.stopPropagation();
-        });
-
-        // Close the mobile menu when clicking elsewhere
-        window.addEventListener('click', (event) => {
-            if (mobileMenu.classList.contains('active') && event.target !== hamburgerIcon) {
-                mobileMenu.classList.remove('active');
-                document.body.style.overflow = 'auto';
-            }
-        });
-
-        // Toggle dropdown menus
-        const dropdownItems = document.querySelectorAll('.navbar-item-drop');
-        dropdownItems.forEach(item => {
-            item.addEventListener('click', (event) => {
-                event.stopPropagation();
-                item.classList.toggle('active');
-                const dropdownContent = item.nextElementSibling;
-                dropdownContent.classList.toggle('active');
-            });
-        });
-    </script>
+    <script src="{{ asset('js/hamburger.js') }}"></script>
 </body>
 </html>
