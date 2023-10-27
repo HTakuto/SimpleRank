756954ae6df9f5082331b0e8aa413ae991e4e52f
HTakuto <htakutolife@gmail.com>
2023-10-27 16:43:12 +0900

    リファクタリング

--- resources/views/partials/hamburger.blade.php
+++ resources/views/partials/hamburger.blade.php
@@ -1,18 +1,22 @@
 <!-- Mobile Menu -->
 <ul class="mobile-menu">
-    <li class="navbar-item navbar-item-drop">言語設定</li>
-    <ul class="dropdown-content">
-        <li><a href="#">Engligsh</a></li>
+    <div class="dropdown">
+        <li class="navbar-item navbar-item-drop">言語設定</li>
+        <ul class="dropdown-content">
+            <li><a href="#">Engligsh</a></li>
             <li><a href="#">日本語</a></li>
-    </ul>
-    <li class="navbar-item">本サイトの説明</li>
+        </ul>
+    </div>
+    <li class="navbar-item"><a href="{{ url('/about') }}">本サイトの説明</a></li>
     <li class="navbar-item">お問い合わせ</li>
-    <li class="navbar-item navbar-item-drop">商品ジャンル</li>
-    <ul class="dropdown-content">
-        <li><a href="#">Category 1</a></li>
-        <li><a href="#">Category 2</a></li>
-        <li><a href="#">Category 3</a></li>
-        <li><a href="#">Category 4</a></li>
-        <li><a href="#">Category 5</a></li>
-    </ul>
+    <div class="dropdown">
+        <li class="navbar-item navbar-item-drop">商品ジャンル</li>
+        <ul class="dropdown-content">
+            <li><a href="#">Category 1</a></li>
+            <li><a href="#">Category 2</a></li>
+            <li><a href="#">Category 3</a></li>
+            <li><a href="#">Category 4</a></li>
+            <li><a href="#">Category 5</a></li>
+        </ul>
+    </div>
 </ul>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -19,11 +19,11 @@
             <div class="dropdown">
                 <span class="navbar-item navbar-item-drop">商品ジャンル</span>
                 <ul class="dropdown-content">
-                    <li><a href="#">test</a></li>
-                    <li><a href="#">test</a></li>
-                    <li><a href="#">test</a></li>
-                    <li><a href="#">test</a></li>
-                    <li><a href="#">test</a></li>
+                    <li><a href="#">Category 1</a></li>
+                    <li><a href="#">Category 2</a></li>
+                    <li><a href="#">Category 3</a></li>
+                    <li><a href="#">Category 4</a></li>
+                    <li><a href="#">Category 5</a></li>
                 </ul>
             </div>
         </div>
