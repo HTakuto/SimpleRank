faf6676ce1f1e8784f255156e92c53b94913f5ca
HTakuto <htakutolife@gmail.com>
2023-10-27 16:43:12 +0900

    headerのリファクタリング

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,8 @@
-Header.blade.php
-{{-- header --}}
+<!-- Header -->
 <header class="header">
     <div class="navbar">
         <div class="navbar-left">
-            <div class="navbar-title">
+            <div class="app-logo">
                 <a href="#">SimpleRank</a>
             </div>
         </div>
@@ -28,27 +27,6 @@ Header.blade.php
                 </div>
             </div>
         </div>
-        <div class="hamburger-icon">&#9776;</div> {{-- ハンバーガーアイコン --}}
-        {{-- <div class="close-icon">&times;</div>  ☓アイコン --}}
+        <div class="hamburger-icon">&#9776;</div>
     </div>
 </header>
-<ul class="mobile-menu">
-    <li class="navbar-item navbar-item-drop">言語設定 </li>
-    <div class="dropdown-content">
-        <a href="#">Engligsh</a>
-        <a href="#">日本語</a>
-    </div>
-    <li class="navbar-item">本サイトの説明</li>
-    <li class="navbar-item">お問い合わせ</li>
-    <li class="dropdown">
-        <span class="navbar-item navbar-item-drop">商品ジャンル </span>
-        <div class="dropdown-content">
-            <a href="#">test</a>
-            <a href="#">test</a>
-            <a href="#">test</a>
-            <a href="#">test</a>
-            <a href="#">test</a>
-        </div>
-    </li>
-</ul>
-
