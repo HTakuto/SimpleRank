f2c9e3f8e3b6bc92217a1be71c37a9b48d0e2be5
HTakuto <htakutolife@gmail.com>
2023-10-27 16:43:12 +0900

    header, jsメイン部分のコンポーネント化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/hamburger.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
-    <title>SimpleRank</title>
+    @include('partials.head')
 </head>
 <body>
     @include('partials.header')
     @include('partials.hamburger')
-    @include('partials.main')
+    @yield('main')
 
-    <script src="{{ asset('js/hamburger.js') }}"></script>
-    <script src="{{ asset('js/carousel.js') }}"></script>
+    @include('partials.js')
 </body>
 </html>
---
+++ resources/views/partials/js.blade.php
@@ -0,0 +1,2 @@
+<script src="{{ asset('js/hamburger.js') }}"></script>
+<script src="{{ asset('js/carousel.js') }}"></script>
