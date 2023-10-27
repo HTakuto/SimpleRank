8b7eab98eb7d87d47407125ecd9399fddee6ad5b
HTakuto <htakutolife@gmail.com>
2023-10-27 16:43:12 +0900

    aboutの追加

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,4 +3,39 @@
 @section('title', 'SimpleRank | about')
 
 @section('main')
+<main>
+    <!-- Hero Section -->
+    <div class="hero_wrapper">
+        <div class="hero_content">
+            <h1 class="hero_title">SimpleRankの説明</h1>
+            <p class="hero_subtitle">SimpleRankの説明に加え、多くよせられる質問を記載しています<br>
+                <div class="hero_bold">強調文字</div>
+            </p>
+        </div>
+    </div>
+
+    <!-- About Section -->
+    <div class="about_wrapper">
+        <div class="about_content_main">
+            <div id="siteDescription" class="content-toggle">
+                <p>SimpleRankとは？</p>
+                <p class="hidden-content">testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+            </div>
+            <h2>このサイト内のリンクは何ですか？</h2>
+            <div id="siteLinks" class="content-toggle">
+                <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+            </div>
+
+            <h2>本サイトはどのような人に向いていますか？</h2>
+            <div id="targetAudience" class="content-toggle">
+                <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+            </div>
+
+            <h2>何を基準にランキングを生成していますか？</h2>
+            <div id="rankingCriteria" class="content-toggle">
+                <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+            </div>
+        </div>
+    </div>
+</main>
 @endsection
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 <link rel="stylesheet" href="{{ asset('css/hamburger.css') }}">
 <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+<link rel="stylesheet" href="{{ asset('css/about.css') }}">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
 
 <title>@yield('title')</title>
