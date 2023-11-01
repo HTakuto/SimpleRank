e140e518a3e0085dc346733f5a3f5edccab05834
HTakuto <htakutolife@gmail.com>
2023-11-02 00:16:01 +0900

    ダミーデータの表示

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -21,98 +21,24 @@
         <div class="ranking_title">
             <h2>１．商品カテゴリ</h2>
         </div>
+
         <div class="ranking_card_wrapper">
             <div class="slide_left_arrow big_arrow">&lt;</div>
-            <!-- カード１ -->
+            @foreach($product_cards as $key => $product_card)
             <div class="ranking_card">
-                <div class="ranking">１</div>
+                <div class="ranking">{{ $key + 1 }}</div>
                 <div class="card">
-                    <h3 class="card_title">商品名</h3>
-                    <img class="thumbnail" src="https://img-c.udemycdn.com/course/480x270/3916498_d47c.jpg" alt="商品画像">
-                    <p class="description">商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！</p>
+                    <h3 class="card_title">{{ $product_card->title }}</h3>
+                    <img class="thumbnail" src="{{ $product_card->image_url }}" alt="商品画像">
+                    <p class="description">{{ $product_card->description }}</p>
                     <ul class="details">
-                        <li class="rating">4.0</li>
-                        <li class="reviews">539</li>
-                        <li class="duration">2時間</li>
-                        <li class="date">2021/06/09</li>
+                        <li class="detail"><i class="fas fa-money-bill"></i> {{ number_format(floor($product_card->price), 0) }}円</li>
+                        <li class="detail"><i class="fas fa-comment"></i> {{ $product_card->review }}</li>
+                        <li class="detail"><i class="fas fa-star"></i> {{ $product_card->star }}</li>
                     </ul>
                 </div>
             </div>
-            <!-- カード２ -->
-            <div class="ranking_card">
-                <div class="ranking">２</div>
-                <div class="card">
-                    <h3 class="card_title">商品名</h3>
-                    <img class="thumbnail" src="https://img-c.udemycdn.com/course/480x270/3916498_d47c.jpg" alt="商品画像">
-                    <p class="description">商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！</p>
-                    <ul class="details">
-                        <li class="rating">4.0</li>
-                        <li class="reviews">539</li>
-                        <li class="duration">2時間</li>
-                        <li class="date">2021/06/09</li>
-                    </ul>
-                </div>
-            </div>
-            <!-- カード３ -->
-            <div class="ranking_card">
-                <div class="ranking">３</div>
-                <div class="card">
-                    <h3 class="card_title">商品名</h3>
-                    <img class="thumbnail" src="https://img-c.udemycdn.com/course/480x270/3916498_d47c.jpg" alt="商品画像">
-                    <p class="description">商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！</p>
-                    <ul class="details">
-                        <li class="rating">4.0</li>
-                        <li class="reviews">539</li>
-                        <li class="duration">2時間</li>
-                        <li class="date">2021/06/09</li>
-                    </ul>
-                </div>
-            </div>
-            <!-- カード４ -->
-            <div class="ranking_card">
-                <div class="ranking">４</div>
-                <div class="card">
-                    <h3 class="card_title">商品名</h3>
-                    <img class="thumbnail" src="https://img-c.udemycdn.com/course/480x270/3916498_d47c.jpg" alt="商品画像">
-                    <p class="description">商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！</p>
-                    <ul class="details">
-                        <li class="rating">4.0</li>
-                        <li class="reviews">539</li>
-                        <li class="duration">2時間</li>
-                        <li class="date">2021/06/09</li>
-                    </ul>
-                </div>
-            </div>
-            <!-- カード５ -->
-            <div class="ranking_card">
-                <div class="ranking">５</div>
-                <div class="card">
-                    <h3 class="card_title">商品名</h3>
-                    <img class="thumbnail" src="https://img-c.udemycdn.com/course/480x270/3916498_d47c.jpg" alt="商品画像">
-                    <p class="description">商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！</p>
-                    <ul class="details">
-                        <li class="rating">4.0</li>
-                        <li class="reviews">539</li>
-                        <li class="duration">2時間</li>
-                        <li class="date">2021/06/09</li>
-                    </ul>
-                </div>
-            </div>
-            <!-- カード6 -->
-            <div class="ranking_card">
-                <div class="ranking">６</div>
-                <div class="card">
-                    <h3 class="card_title">商品名</h3>
-                    <img class="thumbnail" src="https://img-c.udemycdn.com/course/480x270/3916498_d47c.jpg" alt="商品画像">
-                    <p class="description">商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！商品の説明です！</p>
-                    <ul class="details">
-                        <li class="rating">4.0</li>
-                        <li class="reviews">539</li>
-                        <li class="duration">2時間</li>
-                        <li class="date">2021/06/09</li>
-                    </ul>
-                </div>
-            </div>
+            @endforeach
             <div class="slide_right_arrow big_arrow">&gt;</div>
         </div>
     </div>
