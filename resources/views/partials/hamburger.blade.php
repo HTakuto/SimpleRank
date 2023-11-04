d17c192251b13fb59f6c5d4fe108af8a5baa3636
HTakuto <htakutolife@gmail.com>
2023-11-04 15:55:44 +0900

    ハンバーガーメニューの言語切替化

--- resources/views/partials/hamburger.blade.php
+++ resources/views/partials/hamburger.blade.php
@@ -1,16 +1,16 @@
 <!-- Mobile Menu -->
 <div class="mobile-menu">
     <div class="dropdown">
-        <div class="navbar-item navbar-item-drop">言語設定</div>
-        <ul class="dropdown-content">
-            <li><a href="#">Engligsh</a></li>
-            <li><a href="#">日本語</a></li>
-        </ul>
+        <div class="navbar-item navbar-item-drop">{{ trans('messages.language_settings') }}</div>
+        <div class="dropdown-content">
+            <a href="{{ route('setlocale', 'en') }}" class="drop-item">English</a>
+            <a href="{{ route('setlocale', 'ja') }}" class="drop-item">日本語</a>
+        </div>
     </div>
-    <a href="{{ url('/about') }}" class="navbar-item about_link">本サイトの説明</a>
-    <a href="{{ url('/contact') }}" class="navbar-item">お問い合わせ</a>
+    <a href="{{ url('/about') }}" class="navbar-item about_link">{{ trans('messages.about') }}</a>
+    <a href="{{ url('/contact') }}" class="navbar-item">{{ trans('messages.contact') }}</a>
     <div class="dropdown">
-        <div class="navbar-item navbar-item-drop">商品ジャンル</div>
+        <div class="navbar-item navbar-item-drop">{{ trans('messages.product_genre') }}</div>
         <ul class="dropdown-content">
             @foreach ($product_categories as $key => $product_category)
             <li><a href="#">{{ $product_category->name }}</a></li>
