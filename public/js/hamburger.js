3014f5cb5ffb0530c241e5d3aff4632bc9e88f3c
HTakuto <htakutolife@gmail.com>
2023-11-05 12:51:11 +0900

    他箇所のクリックでプルダウンメニューを閉じる記述を追加

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -4,37 +4,49 @@ document.addEventListener('DOMContentLoaded', () => {
     const dropdownItems = document.querySelectorAll('.navbar-item-drop');
 
     if (hamburgerIcon && mobileMenu) {
+        // ハンバーガーアイコンクリック時のイベントハンドラ
         hamburgerIcon.addEventListener('click', (event) => {
             event.stopPropagation();
-            mobileMenu.classList.toggle('active');
+            toggleMobileMenu();
         });
 
+        // ドキュメント全体のクリック時のイベントハンドラ
         window.addEventListener('click', (event) => {
             if (mobileMenu.classList.contains('active') && event.target !== hamburgerIcon) {
                 mobileMenu.classList.remove('active');
                 document.body.style.overflow = 'auto';
             }
+            closeDropdownItems(event.target);
         });
 
+        // ドロップダウン項目のクリック時のイベントハンドラ
         dropdownItems.forEach(item => {
             item.addEventListener('click', (event) => {
                 event.stopPropagation();
-                item.classList.toggle('active');
-                const dropdownContent = item.nextElementSibling;
-                dropdownContent.classList.toggle('active');
-
-                const mobileDropdownContent = item.querySelector('.dropdown-content');
-                if (mobileDropdownContent) {
-                    mobileDropdownContent.classList.toggle('active');
-                }
+                toggleDropdownItem(item);
             });
         });
+    }
 
-        // モバイルメニュー以外をクリックしたときに閉じる
-        window.addEventListener('click', (event) => {
-            if (mobileMenu.classList.contains('active') && event.target !== hamburgerIcon) {
-                mobileMenu.classList.remove('active');
+    // ドロップダウンメニュー以外をクリックしたときに閉じる
+    function closeDropdownItems(target) {
+        dropdownItems.forEach(item => {
+            const dropdownContent = item.nextElementSibling;
+            if (item.classList.contains('active') && target !== item && target !== dropdownContent) {
+                toggleDropdownItem(item);
             }
         });
+    }
+
+    // モバイルメニューの表示/非表示を切り替える
+    function toggleMobileMenu() {
+        mobileMenu.classList.toggle('active');
+    }
+
+    // ドロップダウン項目の表示/非表示を切り替える
+    function toggleDropdownItem(item) {
+        const dropdownContent = item.nextElementSibling;
+        item.classList.toggle('active');
+        dropdownContent.classList.toggle('active');
     }
 });
